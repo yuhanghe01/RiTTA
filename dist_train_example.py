f13b0ca8d0e7bb312eccff8658ad7670758c82e5
Yuhang He (Henry) <11781064+yuhanghe01@users.noreply.github.com>
2025-02-24 11:42:52 -0800

    Add files via upload

--- dist_train_example.py
+++ dist_train_example.py
@@ -2,7 +2,6 @@ import os
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
-import argparse
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler, TensorDataset
 import torch.optim.lr_scheduler as lr_scheduler
@@ -17,27 +16,27 @@ class SimpleModel(torch.nn.Module):
         return self.fc(x)
 
 # Training function
-def train(local_rank, world_size, master_addr, master_port):
-    # Set master address & port
-    os.environ["MASTER_ADDR"] = master_addr
-    os.environ["MASTER_PORT"] = master_port
+def train():
+    # Initialize distributed environment variables (set by torchrun)
+    rank = int(os.environ["RANK"])            # Global rank of the process
+    world_size = int(os.environ["WORLD_SIZE"])  # Total number of processes
+    local_rank = int(os.environ["LOCAL_RANK"])  # Rank within the current node
 
-    # Retrieve global rank and local rank
-    rank = int(os.environ["RANK"])  # Global rank
-    torch.cuda.set_device(local_rank)  # Assign correct GPU based on local rank
+    # Set device
+    torch.cuda.set_device(local_rank)
 
-    # Initialize the process group
+    # Initialize process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
     # Create model and move to GPU
     model = SimpleModel(input_size=10, output_size=2).to(local_rank)
-    model = DDP(model, device_ids=[local_rank], find_unused_parameters=False)  
+    model = DDP(model, device_ids=[local_rank], find_unused_parameters=False)
 
     # Define loss function and optimizer
     criterion = torch.nn.CrossEntropyLoss().to(local_rank)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-    # Add LR Scheduler
+    # Learning rate scheduler
     scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
     # Create dummy dataset
@@ -75,18 +74,5 @@ def train(local_rank, world_size, master_addr, master_port):
     # Cleanup
     dist.destroy_process_group()
 
-# Entry point
-def main():
-    parser = argparse.ArgumentParser(description="Distributed Training Example")
-    parser.add_argument("--master_addr", type=str, default="localhost", help="Master node address")
-    parser.add_argument("--master_port", type=str, default="12355", help="Master node port")
-    args = parser.parse_args()
-
-    world_size = int(os.environ["WORLD_SIZE"])  # Get world size from environment
-    local_rank = int(os.environ["LOCAL_RANK"])  # Local rank within the node
-
-    # Spawn processes on each GPU of the node
-    mp.spawn(train, args=(world_size, args.master_addr, args.master_port), nprocs=torch.cuda.device_count(), join=True)
-
 if __name__ == "__main__":
-    main()
+    train()
