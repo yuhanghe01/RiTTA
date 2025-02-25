85a16ef31ad75fb0e42a95d0973ba086e895cd89
Yuhang He (Henry) <11781064+yuhanghe01@users.noreply.github.com>
2025-02-25 15:47:12 -0800

    Add files via upload

--- dist_train_example.py
+++ dist_train_example.py
@@ -78,9 +78,11 @@ def train():
             current_lr = scheduler.get_last_lr()[0]
             print(f"Epoch {epoch+1}/10, Loss: {loss.item():.4f}, LR: {current_lr:.6f}")
 
-    # Save the model only from rank 0
+    Save the model only from rank 0
     if rank == 0:
         save_path = '/mnt/pvc-blob-fuse-out/yuhang/checkpoint.pth'
+        if not os.path.exists('/mnt/pvc-blob-fuse-out/yuhang/'):
+            os.makedirs('/mnt/pvc-blob-fuse-out/yuhang/')
         torch.save(model.module.state_dict(), save_path)
         print(f"Model saved at {save_path}")
 
